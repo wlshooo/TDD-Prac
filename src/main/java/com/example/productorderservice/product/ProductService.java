3d26332609824b56c66cd3f2f3e924d530911cb9
wlshooo <kswlsgh@naver.com>
2024-08-16 04:47:04 +0900

    replace JPA

--- src/main/java/com/example/productorderservice/product/ProductService.java
+++ src/main/java/com/example/productorderservice/product/ProductService.java
@@ -1,6 +1,7 @@
 package com.example.productorderservice.product;
 
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 class ProductService {
@@ -10,6 +11,7 @@ class ProductService {
         this.productPort = productPort;
     }
 
+    @Transactional
     public void addProduct(AddProductRequest request) {
         final Product product = new Product(request.name(), request.price(), request.discountPolicy());
 
