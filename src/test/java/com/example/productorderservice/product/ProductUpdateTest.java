ac00d4f9530f18c70b8dd07e0f4ed25547da7d64
wlshooo <kswlsgh@naver.com>
2024-08-17 02:09:59 +0900

    replace pojo ->springboot test

--- src/test/java/com/example/productorderservice/product/ProductUpdateTest.java
+++ src/test/java/com/example/productorderservice/product/ProductUpdateTest.java
@@ -1,50 +1,28 @@
 package com.example.productorderservice.product;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@ExtendWith(MockitoExtension.class)
+@SpringBootTest
 public class ProductUpdateTest {
 
+    @Autowired
     private ProductService productService;
-    private ProductPort productPort;
-
-    @BeforeEach
-    void setUp() {
-        productPort = Mockito.mock(ProductPort.class);
-        productService = new ProductService(productPort);
-    }
 
     @Test
     void 상품수정() {
+        productService.addProduct(ProductSteps.상품등록요청_생성());
         final Long productId = 1L;
         final UpdateProductRequest request = new UpdateProductRequest("상품 수정", 2000, DiscountPolicy.NONE);
-        final Product product = new Product("상품명",1000,DiscountPolicy.NONE);
-        Mockito.when(productPort.getProduct(productId)).thenReturn(product);
 
 
         productService.updateProduct(productId, request);
 
-        assertThat(product.getName()).isEqualTo("상품 수정");
-        assertThat(product.getPrice()).isEqualTo(2000);
+        GetProductResponse product = productService.getProduct(productId);
+        assertThat(product.name()).isEqualTo("상품 수정");
+        assertThat(product.price()).isEqualTo(2000);
     }
-
-//    private static class StubProductPort implements ProductPort {
-//        public Product getProduct_will_return;
-//
-//        @Override
-//        public void save(Product product) {
-//
-//        }
-//
-//        @Override
-//        public Product getProduct(Long productId) {
-//            return getProduct_will_return;
-//        }
-//    }
 }
