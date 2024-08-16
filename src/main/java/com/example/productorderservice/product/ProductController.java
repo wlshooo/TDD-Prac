48404be70a4ec78b0bf5089c4853ec3f01247718
wlshooo <kswlsgh@naver.com>
2024-08-17 01:17:41 +0900

    product read func impl

--- src/main/java/com/example/productorderservice/product/ProductController.java
+++ src/main/java/com/example/productorderservice/product/ProductController.java
@@ -1,12 +1,11 @@
 package com.example.productorderservice.product;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/products")
@@ -23,5 +22,12 @@ public class ProductController {
         productService.addProduct(request);
 
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping("/{productId}")
+    public ResponseEntity<GetProductResponse> getProduct(@PathVariable final Long productId) {
+        GetProductResponse productResponse = productService.getProduct(productId);
+
+        return ResponseEntity.status(HttpStatus.OK).body(productResponse);
     }
 }
